c271c3477541f9e40df9cc04fe0f9c1a82cb1f1b
Christoph Schrade <schrade.christoph@gmail.com>
2023-07-28 12:26:09 +0200

    consider only data which is at most 46 days old

--- algo/operator.py
+++ algo/operator.py
@@ -184,8 +184,8 @@ class Operator(util.OperatorBase):
     
     def run(self, data, selector='energy_func'):
         self.timestamp = self.todatetime(data['Time']).tz_localize(None)
-        #if pd.Timestamp.now() - self.timestamp > pd.Timedelta(17,'d'):
-            #return
+        if pd.Timestamp.now() - self.timestamp > pd.Timedelta(46,'d'):
+            return
         print('energy: '+str(data['Consumption'])+'  '+'time: '+str(self.timestamp))
         if list(self.data_history.index) and self.timestamp <= self.data_history.index[-1]:
             return
